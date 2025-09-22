7ffe854809fa45fe15687af9e29d613f4f7e5883
Onekamardin <105882875+Onekamardin@users.noreply.github.com>
2025-09-22 21:27:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static int calculateCustoms(int price,int weight) {
+        int result = price/100+weight*100;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите цену товара (в руб.):");
+        int price = scanner.nextInt();
+        System.out.print("Введите вес товара (в кг.):");
+        int weight = scanner.nextInt();
+        int duty  = calculateCustoms(price,weight);
+        System.out.println("Размер пошлины (в руб.) составит: "+ duty);
+    }
+}
